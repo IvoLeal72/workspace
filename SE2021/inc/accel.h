8700716f6c44e745dceb8f76e7f4f8d49e3db5d5
LoyaltyGamer72 <ivocleal@gmail.com>
2021-01-12 15:35:24 +0000

    Created Accelerometer library

---
+++ SE2021/inc/accel.h
@@ -0,0 +1,15 @@
+/*
+ * accel.h
+ *
+ *  Created on: 12/01/2021
+ *      Author: ivo
+ */
+
+#ifndef ACCEL_H_
+#define ACCEL_H_
+
+#include "spi.h"
+
+void Accel_Init(void);
+
+#endif /* ACCEL_H_ */
---
+++ SE2021/src/accel.c
@@ -0,0 +1,12 @@
+/*
+ * accel.c
+ *
+ *  Created on: 12/01/2021
+ *      Author: ivo
+ */
+
+#include "accel.h"
+
+void Accel_Init(void){
+
+}
