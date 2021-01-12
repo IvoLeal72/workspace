b4edacae241f70346096e8b5cb6b08f64e20adfd
LoyaltyGamer72 <ivocleal@gmail.com>
2021-01-12 20:02:34 +0000

    updated led library for new board

--- SE2021/src/led.c
+++ SE2021/src/led.c
@@ -15,9 +15,9 @@ void LED_Init(bool state){
 	LPC_PINCON->PINSEL1 &= ~(3<<12);
 	LPC_GPIO0->FIODIR |= (1<<22);
 	if(state)
-		LPC_GPIO0->FIOSET=1<<22;
-	else
 		LPC_GPIO0->FIOCLR=1<<22;
+	else
+		LPC_GPIO0->FIOSET=1<<22;
 }
 
 bool LED_GetState(void){
@@ -25,9 +25,9 @@ bool LED_GetState(void){
 }
 
 void LED_On(void){
-	LPC_GPIO0->FIOSET=1<<22;
+	LPC_GPIO0->FIOCLR=1<<22;
 }
 
 void LED_Off(void){
-	LPC_GPIO0->FIOCLR=1<<22;
+	LPC_GPIO0->FIOSET=1<<22;
 }
