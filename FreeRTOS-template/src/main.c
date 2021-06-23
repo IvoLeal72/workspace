ea7638b968864b40930e6d5a96bf3e5411af12b3
LoyaltyGamer72 <ivocleal@gmail.com>
2021-06-23 22:50:24 +0100

    Tried testing esp with taks, not working

--- FreeRTOS-template/src/main.c
+++ FreeRTOS-template/src/main.c
@@ -9,6 +9,8 @@
 #include "queue.h"
 #include "led.h"
 #include "lcd.h"
+#include "esp8266.h"
+#include "wait.h"
 
 QueueHandle_t xQueue;
 
@@ -28,18 +30,44 @@ void timerTask(void* params){
 	char a='4';
 
 	for(;;){
-		vTaskDelay(pdMS_TO);
+		//vTaskDelay(pdMS_TO);
 		xQueueSend(xQueue, &a, -1);
 	}
 }
 
+void testTask(void* params){
+	WAIT_Init();
+	LED_Init(0b100);
+	LED_SetState(ESP_Init());
+	/*WIFI_NETWORK wifi_list[40];
+	wifi_list[0].secur=6;
+	int res=ESP_ListAp(wifi_list, 40);
+	if(res<0) printf("retornou erro %d\n", res);
+	putchar(10);
+	for(int i=0; i<res; i++){
+		printf("%d ; name: %s ; mac: %s\n", wifi_list[i].secur, wifi_list[i].ssid, wifi_list[i].mac);
+	}*/
+	WIFI_NETWORK con;
+	strcpy(con.ssid, "NOS-LEAL 2");
+	strcpy(con.pwd, "leal697121");
+	con.secur=4;
+	char *testStr="123456789\n123456789\n123456789\n123456789\n123456789\n123456789\n123456789\n123456789\n123456789\n123456789\n123456789\n123456789\n123456789\n123456789\n123456789\n123456789\n123456789\n123456789\n123456789\n123456789\n";
+	printf("%d\n", ESP_SetAp(con, false));
+	printf("%s\n", ESP_RemoteStart("TCP", "192.168.1.17", 54321)?"TRUE":"FALSE");
+	printf("%s\n", ESP_RemoteSend(testStr, strlen(testStr))?"TRUE":"FALSE");
+	LED_SetState(0);
+	while(!ESP_WaitForIPD(60000));
+	LED_SetState(0b10);
+	ESP_DATA* data=NULL;
+	data=ESP_RemoteReceive();
+	printf("%s\n", ESP_RemoteStop()?"TRUE":"FALSE");
+	printf("%s\n", memcmp(testStr, data->buffer, strlen(testStr))==0?"TRUE":"FALSE");
+}
+
 int main(void)
 {
-	LED_Init(false);
-	xQueue=xQueueCreate(1,sizeof(unsigned int));
 
-	xTaskCreate(ledTask, "LedTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-	xTaskCreate(timerTask, "LedTask", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+	xTaskCreate(testTask, "testTask", 64*configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 	/* Create tasks */
 	vTaskStartScheduler();
 	return 0;
